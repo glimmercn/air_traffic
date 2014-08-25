52f8cc84141b8f11d075b3de54434a74316632b9
kan <huangkandiy@gmail.com>
2014-08-25 12:39:09 -0400

    add interpolation of path

--- accessery.py
+++ accessery.py
@@ -6,6 +6,8 @@ Created on Jun 22, 2014
 
 import numpy
 from shapely.geometry import *
+from pylab import *
+import matplotlib.pyplot as plt
 
 class Trj:
   '''
@@ -18,50 +20,64 @@ class Trj:
     Constructor
     '''
     self.nodes = ns
+  def draw(self, m, c):
+    plt.plot(*zip(*self.nodes), marker=m, color=c)
+
 
 class Graph:
   '''
   graph class
   '''
- def __init__(self, points, im):
-   '''
-   :param points:
-   :param im: incident matrix
-   :return:
-   '''
-   self.points = points
-   self.im = im
-   self.pn = len(points)
+  def __init__(self, points, im):
+    self.points = points
+    self.im = im
+    self.pn = len(points)
 
-def heter_interpolate(p1, p2, n, rg, params):
+
+
+def eclidean(p1, p2):
+  return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**.5
+
+def interpolate_edge(p1, p2, n):
   '''
   insert n points between p1, p2
   :param p1:
   :param p2:
   :param n:
-  :param rg: random number generator
-  :param params: the parameters of rg
   :return:
   '''
   ps = []
-  if p1.x != p2.x:
-    k = (p2.y - p1.y)/(p2.x - p1.x)
-    pnew = Point(p1)
-
+  cp1 = p1[:]
+  if p1[0] != p2[0]:
+    k = (p2[1] - p1[1])/(p2[0]- p1[0])
+    Dx = (p2[0] - p1[0])/(n+1)
     for i in range(n):
-      dx = rg(params)
-      dy = k * rg(params)
-      pnew.x += dx
-      pnew.y += dy
-      ps.append(Point(pnew))
+      dx = Dx * (i+1)
+      dy = k * dx
+      pi = [cp1[0] + dx, cp1[1]+dy]
+      ps.append(pi)
     return ps
   else:
-    pnew = Point(p1)
-    for i in range(n):
-      dy = rg(params)
-      pnew.y += dy
-      ps.append(Point(pnew))
-    return ps
+    ps = [[p1[0], p1[1] + (i+1)*(p2[1]-p1[1])/(n+1)] for i in range(n)]
+  return ps
+
+def interpolate_path(path, slen):
+  '''
+  interpolate the path by a given length
+  :param path:
+  :param slen:the length of the step
+  :return:interpolated path(trajectory before perturbation)
+  '''
+  trj = [path[0]]
+  for i in range(len(path)-1):
+    p1 = path[i]
+    p2 = path[i+1]
+    l = eclidean(p1, p2)
+    n = int(l/slen)
+    trj += interpolate_edge(p1, p2, n)
+    trj.append(p2)
+  return Trj(trj)
+
 
 def random_graph(n, p=0.5):
   '''
@@ -77,15 +93,8 @@ def random_graph(n, p=0.5):
       if numpy.random.random_sample() < p:
         im[i].append(j)
         im[j].append(i)
-  return points,im
+  return Graph(points, im)
 
-def add_uniform_noise(nx, ny):
-  '''
-  add uniform noise to trajectory
-  :param nx: noise on x
-  :param ny: noise on y
-  :return: trajectory with noise
-  '''
 def add_noise(ps, xng, xp, yng = None, yp = None):
   '''
   add noise to a path
@@ -142,14 +151,15 @@ def random_path(g, l, s = None):
   :return:
   '''
   if s == None:
-    s = numpy.random.randomint(0, g.pn)
+    s = numpy.random.randint(0, g.pn)
   p = s
-  path = [p]
+  path = [g.points[p]]
   for i in range(l):
-     j = numpy.random.randomint(0, len(g.im[p]))
+     j = numpy.random.randint(0, len(g.im[p]))
      p = g.im[p][j]
-     path.append(p)
+     path.append(g.points[p])
   return path
+
 
 def shortest_path(g, s, t):
   '''
@@ -161,6 +171,16 @@ def shortest_path(g, s, t):
   '''
   pass
 
+'''distributions'''
+def scaled_unif(scale):
+  return numpy.random.ranf()*scale
+
+if __name__ == "__main__":
+  g= random_graph(15, 1)
+  path = random_path(g, 4)
+  trj = interpolate_path(path, 0.02)
+  add_noise(trj.nodes, scaled_unif, [0.01])
+  trj.draw('o','r')
 
 
 
