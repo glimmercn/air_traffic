f92552ea6ab492978d8bf86bbc4bd71dbdde3d60
kan <huangkandiy@gmail.com>
2014-08-26 17:32:57 -0400

    change examples a little.

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,6 @@
 from accessory import *
 import copy
-example = 3
+example = 2
 
 if example == 1:
   g= random_graph(15, 1)
@@ -9,7 +9,7 @@ if example == 1:
     trj = interpolate_path(path, 0.03)
     add_noise(trj.nodes, scaled_unif, [0.02])
     trj.draw('.','r')
-  for i in range(10):
+  for i in range(15):
     path = random_path(g, numpy.random.randint(2, 6))
     trj = interpolate_path(path, 0.03)
     add_noise(trj.nodes, scaled_unif, [0.05])
@@ -17,16 +17,16 @@ if example == 1:
   plt.show()
 
 if example == 2:
-  g= random_graph(15, 1)
+  g= random_graph(20, 1)
   straight_trjs = []
-  for i in range(18):
-    path = random_path(g, numpy.random.randint(2, 6))
+  for i in range(10):
+    path = random_path(g, numpy.random.randint(2, 5))
     straight_trjs.append(interpolate_path(path, 0.03))
   low_noise_trjs, high_noise_trjs = [], []
   for trj in straight_trjs:
     t1 = copy.deepcopy(trj)
     t2 = copy.deepcopy(trj)
-    add_noise(t1.nodes, scaled_unif, [0.01])
+    add_noise(t1.nodes, scaled_unif, [0.02])
     add_noise(t2.nodes, scaled_unif, [0.05])
     low_noise_trjs.append(t1)
     high_noise_trjs.append(t2)
@@ -37,16 +37,16 @@ if example == 2:
   plt.show()
 
 if example == 3:
-  g= random_graph(15, 1)
+  g= random_graph(20, 1)
   straight_trjs = []
-  for i in range(9):
-    path = random_path(g, numpy.random.randint(2, 6))
+  for i in range(10):
+    path = random_path(g, numpy.random.randint(2, 5))
     straight_trjs.append(interpolate_path(path, 0.03))
   low_noise_trjs, high_noise_trjs = [], []
   for trj in straight_trjs:
     t1 = copy.deepcopy(trj)
     t2 = copy.deepcopy(trj)
-    weight = [1, 0.5]
+    weight = [1, 0.5, 0.3, 0.1]
     add_cum_noise(weight, weight,t1.nodes, scaled_unif, [.02])
     add_cum_noise(weight, weight,t2.nodes, scaled_unif, [.05])
     low_noise_trjs.append(t1)
