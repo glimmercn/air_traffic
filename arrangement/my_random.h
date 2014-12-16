a5fa20da0a25f2f10eb4125690d44dcedae1e074
kan <huangkandiy@gmail.com>
2014-12-16 13:47:48 +0800

    random_pick is function to simulate a discrete distribution

--- arrangement/my_random.h
+++ arrangement/my_random.h
@@ -1,4 +1,22 @@
 #include <vector>
-
+#include <stdlib.h>
 typedef std::vector<int>                Distribution;
-typedef std::vector<Distribution>       Distributions;
+
+int random_pick(const Distribution& dtb) {
+  int up = 0;
+  int sum = 0;
+  for (int i = 0; i < dtb.size(); i++) {
+    sum += dtb[i];
+  }
+
+  float p = rand() / RAND_MAX * sum;
+
+  for (int i = 0; i < dtb.size(); i++) {
+    up += dtb[i];
+    if (p < up)
+      return i;
+  }
+
+  return dtb.size() - 1; 
+
+}
