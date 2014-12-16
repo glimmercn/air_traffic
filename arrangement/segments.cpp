1988667983e85988828b5b469ac1a713d3293509
kan <huangkandiy@gmail.com>
2014-12-16 14:12:30 +0800

    change the experiment

--- arrangement/segments.cpp
+++ arrangement/segments.cpp
@@ -9,18 +9,17 @@ using namespace std;
 int main(int argc, const char *argv[])
 {
   Arrangement_2 arr;
-  vector<Segment_2> segs = random_arrangement(arr, 30);
-  //std::cout <<arr.number_of_edges()  << std::endl;
-
-  Arrangement_2::Vertex_const_iterator vh = arr.vertices_begin();
-  push_iter(vh, 10);  
-
-  std::vector<Vertex_const_handle> path = random_simple_path(arr, 10, vh);
-
-  output_path(path, "helloworld");
-  output_arrangement(segs, "hellohuman");
+  vector<Segment_2> segs = random_arrangement(arr, 20);
+  weighted_route(arr, one_major_dtb);
+//  Arrangement_2::Vertex_const_iterator vh = arr.vertices_begin();
+//  push_iter(vh, 10);  
+//
+//  std::vector<Vertex_const_handle> path = random_simple_path(arr, 10, vh);
+//
+//  output_path(path, "helloworld");
+  output_arrangement(segs, "arr.data");
   
-  append("50", "paths.txt");
+  append("50", "paths.data");
 
   int nPath = 50;
   
@@ -31,9 +30,9 @@ int main(int argc, const char *argv[])
     Arrangement_2::Vertex_const_iterator vh = arr.vertices_begin();
     push_iter(vh, nStep);  
     
-    int nLen = rand() % 7 + 2; 
+    int nLen = rand() % 11 + 2; 
     Path path = random_simple_path(arr, nLen, vh);
-    output_path(path, "paths.txt");   
+    output_path(path, "paths.data");   
   }
 
   return 0;
