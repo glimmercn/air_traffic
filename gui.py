90204ff05237b9a3ac03727e10d4e6d563fcb7a4
Kan Huang <huangkandiy@gmail.com>
2015-04-02 14:59:26 -0400

    warning dialog

--- gui.py
+++ gui.py
@@ -35,25 +35,26 @@ class MainWindow(QtGui.QWidget):
       self.loadEdit = QtGui.QLineEdit("arrangement/100_simple.paths")
 
       noiseLabel = QtGui.QLabel('Noise Name:')
-      self.noiseEdit = QtGui.QLineEdit("Put the name of noise here")
+      self.noiseEdit = QtGui.QLineEdit("Put a number here.")
 
       self.truncButton = QtGui.QPushButton('Truncate')
       self.connect(self.truncButton, QtCore.SIGNAL('pressed()'), self.truncate)
 
-      self.NoiseButton = QtGui.QPushButton('Add Noise')
-      self.connect(self.NoiseButton, QtCore.SIGNAL('pressed()'), self.addNoise)
+      self.noiseButton = QtGui.QPushButton('Add Noise')
+      self.connect(self.noiseButton, QtCore.SIGNAL('pressed()'), self.addNoise)
+
 
       grid = QtGui.QGridLayout()
       grid.setSpacing(10)
 
       grid.addWidget(self.loadButton, 1, 0)
       grid.addWidget(self.loadEdit, 1, 1)
-      #grid.addWidget(self.noiseEdit, 3, 1)
       grid.addWidget(self.drawPathsButton, 2, 0)
 
       #grid.addWidget(noiseLabel, 3, 0)
       grid.addWidget(self.truncButton, 4, 0)
-      grid.addWidget(self.NoiseButton, 5, 0)
+      grid.addWidget(self.noiseButton, 5, 0)
+      grid.addWidget(self.noiseEdit, 5, 1)
 
       grid.addWidget(self.canvas, 6, 1, 8, 1)
 
@@ -68,7 +69,11 @@ class MainWindow(QtGui.QWidget):
     def loadPath(self):
       global trajecotries, pathSet
       pathFileName = self.loadEdit.text()
-      pathSet = read_trjs(pathFileName)
+      try:
+        pathSet = read_trjs(pathFileName)
+      except IOError:
+        QtGui.QMessageBox.warning(self, "Error", "can't find " + pathFileName)
+
       ''' the paths will be interpolated immediately.
       pathSet is a copy for reset.
       trjSet is a copy that functions will work on.
@@ -85,7 +90,7 @@ class MainWindow(QtGui.QWidget):
 
     def addNoise(self):
       global trajecotries, pathSet
-      params = [1]
+      params = [float(self.noiseEdit.text())]
       trajecotries.add_noise(uniform_square_noise, params)
       self.canvas.repaint()
 
@@ -103,17 +108,17 @@ class Canvas(QtGui.QWidget):
         self.show()
 
     def paintEvent(self, e):
-
-        qp = QtGui.QPainter()
-        qp.begin(self)
+      global trajecotries
+      qp = QtGui.QPainter()
+      qp.begin(self)
+      if trajecotries:
         self.drawTrj(qp)
-        qp.end()
+      qp.end()
 
     def drawTrj(self, qp):
       global trajecotries
       pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
       qp.setPen(pen)
-
       for trj in trajecotries.trjs:
         trj.gui_draw(qp)
 
